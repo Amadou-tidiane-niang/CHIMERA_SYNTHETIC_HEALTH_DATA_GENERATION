1be5dba4c4b2329772755abd1636435df640b783
Amadou-tidiane.niang <amadoutidiane.niang93@gmail.com>
2026-06-01 10:01:36 +0200

    Update 1_preprocessing.R

--- scripts/1_preprocessing.R
+++ scripts/1_preprocessing.R
@@ -127,139 +127,20 @@ row.names(AIDS_hol) <- NULL
 write.csv(AIDS_trn, here("data", "AIDS_trn.csv"), row.names = FALSE)
 write.csv(AIDS_hol, here("data", "AIDS_hol.csv"), row.names = FALSE)
 
-
 # ============================================================
-# DATA LOADING | REIN 
+# DATA LOADING | AIDS 
 # ============================================================
-data <- read_excel(here("data", "cohorte_incident_2012-2021.xlsx"))
-data_comp <- read_excel(here("data", "complement_janv2025.xlsx"))
+df_rein_with_na <- read.csv(here("data", "df_rein_with_na.csv"))
 
-# merge
-data <- merge(data,data_comp,by="id_ano",all.x="TRUE")
-str(data)
-
-# filter
-df <- data|>
-  dplyr::select(-c(EQD_REG_LIB,res_dep_cod,NEPH_COD,id_ano,liste_longue))
-
-# Transform all character variables into factor
-df <- df|>mutate_if(is.character,as.factor)
-
-# Data Management
-df <- df %>%
-  mutate(sex = as.factor(df$sex),
-         URGn = as.factor(df$URGn),
-         KTTINIn = as.factor(df$KTTINIn),
-         PBRn = as.factor(df$PBRn),
-         IDEn = as.factor(df$IDEn),
-         DIABn = as.factor(df$DIABn),
-         IRCn = as.factor(df$IRCn),
-         O2n = as.factor(df$O2n),
-         SASn = as.factor(df$SASn),
-         ICn = as.factor(df$ICn),
-         IDMn = as.factor(df$IDMn),
-         RYTHMn = as.factor(df$RYTHMn),
-         ANEVn = as.factor(df$ANEVn), 
-         AMIn = as.factor(df$AMIn),
-         AVCAITn = as.factor(df$AVCAITn),
-         KCn = as.factor(df$KCn),
-         VHBn = as.factor(df$VHBn),
-         VHCn= as.factor(df$VHCn),
-         CIRHn = as.factor(df$CIRHn),
-         HANDn = as.factor(df$HANDn),
-         AMPn = as.factor(df$AMPn),
-         PLEGn = as.factor(df$PLEGn),
-         CECITEn = as.factor(df$CECITEn),
-         COMPORTn = as.factor(df$COMPORTn),
-         sero = as.factor(df$sero),
-         coro = as.factor(df$coro),
-         ICOROn= as.factor(df$ICOROn))
-
-
-
-
-# the end date of the study
-date_cens_adminis <- "2022-12-31"
-
-# compute survival time and censored time
-df <- prepare_survival_data_time(df,date_cens_adminis,"DDIRT",c("dgrf","dsvr","dpdv","ddc"),censored_compute = TRUE)
-
-
-# remove all variables related to survival time and censoring
-df <- df|>
-  dplyr::select(-c(DDIRT,dgrf,dsvr,ddc,dpdv,DINSCMED))
-
-# Variable with % NAs less than 50
-variables_names <- names(df)[colMeans(is.na(df)) < 0.5]
-df <- df[,variables_names]
-
-# Rename variables
-df <- df %>%
-  rename(
-    Age=age,
-    Sex=sex,
-    Serum_albumin_level=ALBINI,
-    Body_mass_index=bmi,
-    eGFR=CKEPI,
-    Diabetes=DIABn,
-    Heart_failure=STADICn,
-    Peripheral_artery_disease=STDAMIn,
-    Coronary_artery_disease=ICOROn,
-    Myocardial_infarction=IDMn,
-    Stroke=AVCAITn,
-    Cardiac_arrhythmia=RYTHMn,
-    Chronic_respiratory_failure=IRCn,
-    Malignancy=KCn,
-    Cirrhosis=CIRHn,
-    Severe_behavioral_disorders=COMPORTn,
-    Walking_autonomy=MARCHn,
-    Censored=censored
-  )
-
-# Select and rename relevant variables from the original dataset
-df <- df |>
-  dplyr::select(c(Age, Sex, Serum_albumin_level,Body_mass_index,eGFR,Diabetes, Heart_failure,
-                  Peripheral_artery_disease, Coronary_artery_disease, Myocardial_infarction, Stroke,
-                  Cardiac_arrhythmia, Chronic_respiratory_failure, Malignancy, Cirrhosis,
-                  Severe_behavioral_disorders, Walking_autonomy, times, Censored))
-
-# convert factor
-df <- df|>
-  mutate(
-    Diabetes = factor(Diabetes, levels = c(0, 1), labels = c("No", "Yes")),
-    Sex = factor(Sex, levels = c("Femme", "Homme"), labels = c("Female", "Male")),
-    Heart_failure=factor(Heart_failure,levels=c("Pas d'insuffisance cardiaque","Insuffisance cardiaque Stade I-II","Insuffisance cardiaque Stade III-IV"),labels=c("None","NYHA I–II","NYHA III–IV")),
-    Peripheral_artery_disease=factor(Peripheral_artery_disease,levels=c("Pas d'artérite des membres inférieurs","Artérite des membres inférieurs stade I-II","Artérite des membres inférieurs stade III-IV"),labels=c("None", "stage I-II", "stage III-IV")),
-    Coronary_artery_disease= factor(Coronary_artery_disease , levels = c(0, 1), labels = c("No", "Yes")),
-    Myocardial_infarction=factor(Myocardial_infarction, levels = c(0, 1), labels = c("No", "Yes")),
-    Stroke=factor(Stroke, levels = c(0, 1), labels = c("No", "Yes")),
-    Cardiac_arrhythmia=factor(Cardiac_arrhythmia, levels = c(0, 1), labels = c("No", "Yes")),
-    Chronic_respiratory_failure=factor(Chronic_respiratory_failure, levels = c(0, 1), labels = c("No", "Yes")),
-    Malignancy=factor(Malignancy, levels = c(0, 1), labels = c("No", "Yes")),
-    Cirrhosis=factor(Cirrhosis, levels = c(0, 1), labels = c("No", "Yes")),
-    Severe_behavioral_disorders=factor(Severe_behavioral_disorders, levels = c(0, 1), labels = c("No", "Yes")),
-    Walking_autonomy=factor(Walking_autonomy,levels=c("Marche autonome","Nécessité d'une tierce personne","Incapacité totale"),labels=c("Independent","Assisted","Unable"))
-)
-    
-df <- df[which(df$Age>=75),]
-row.names(df) <- NULL
-
-na_indices_initial <- which(is.na(df), arr.ind = TRUE)
-REIN_CHIMERA[na_indices_initial] <- NA
-df_rein_with_na <- REIN_CHIMERA
-
-df <- REIN_CHIMERA
-# Save dataset
-write.csv(df_rein_with_na, here("data", "df_rein_with_na.csv"), row.names = FALSE)
 
 # --- Initial MICE setup
-ini <- mice(df, maxit = 0)
+ini <- mice(df_rein_with_na, maxit = 0)
 meth <- ini$method
 pred <- ini$predictorMatrix
-meth[names(df)[sapply(df, is.numeric)]] <- "pmm"
+meth[names(df_rein_with)[sapply(df_rein_with_na, is.numeric)]] <- "pmm"
 
 # --- Impute missing data
-df_rein_without_na <- complete(mice(df, m = 1, maxit = 5, seed = 123,method = meth, predictorMatrix = pred, printFlag = TRUE))
+df_rein_without_na <- complete(mice(df_rein_with_na, m = 1, maxit = 5, seed = 123,method = meth, predictorMatrix = pred, printFlag = TRUE))
 
 # Save dataset
 write.csv(df_rein_without_na, here("data", "df_rein_without_na.csv"), row.names = FALSE)
